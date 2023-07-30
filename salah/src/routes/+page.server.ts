c3a85ab8b1463fd6506c5728fc9fe96559005274
sharno <sharnoby3@gmail.com>
2023-07-29 22:52:19 -0400

    remove unused code

--- salah/src/routes/+page.server.ts
+++ salah/src/routes/+page.server.ts
@@ -1,31 +1,6 @@
-import type { Actions, PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
 	const ip = event.getClientAddress();
 	console.log(ip);
 };
-
-export const actions: Actions = {
-	async default({ request }) {
-		const data = await request.json();
-		console.log(data);
-		const timings = await fetchTimings(data.city, data.country);
-		const qibla = await fetchQibla(data.latitude, data.longitude);
-
-		return { timings, qibla };
-	}
-};
-
-async function fetchTimings(city: string, country: string) {
-	const url = `https://api.aladhan.com/v1/timingsByCity/29-07-2023?city=${city}&country=${country}`;
-	const res = await fetch(url);
-	const { data } = await res.json();
-	return data;
-}
-
-async function fetchQibla(latitude: number, longitude: number) {
-	const url = `https://api.aladhan.com/v1/qibla/${latitude}/${longitude}`;
-	const res = await fetch(url);
-	const { data } = await res.json();
-	return data;
-}
